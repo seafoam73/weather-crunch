0b3eb63bf0370ddb75eac9732aa40c49fd8a8200
seafoam73 <jgilmore2012@gmail.com>
2026-05-03 22:01:23 -0500

    Added extract_weather as the parameter for transform_weather in load.py

--- src/load.py
+++ src/load.py
@@ -2,13 +2,18 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
+from transform import transform_weather
 
+# Load environment variables from .env
 load_dotenv()
 
+#retreives DATABASE_URL from .env
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+#uses SQLalchemy to translate between pandas and PostgreSQL. Sets up the instructions to connect to to_sql()
 engine = create_engine(DATABASE_URL)
 
-#where is df coming from?
+#calls the transform.py transformed data and puts them in the respective SQL tables
 def weather_load():
     df = transform_weather()
     df.to_sql(name="dim_city", con=engine, if_exists='append', index=False)
@@ -17,6 +22,5 @@ def weather_load():
     df.to_sql(name="fact_event", con=engine, if_exists='append', index=False)
 
 if __name__ == "__main__":
-    sample = 
-    
-    print()
+    weather_load()
+    print("Data loaded successfully!")
