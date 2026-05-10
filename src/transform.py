aaa84a680586fed146eedd1ea76c4e547b457d17
seafoam73 <jgilmore2012@gmail.com>
2026-05-10 18:40:14 -0500

    added geo data parameter to the transform_weather function

--- src/transform.py
+++ src/transform.py
@@ -1,28 +1,33 @@
 import pandas as pd
 from datetime import datetime
 
-#takes the 7 fiels we want and pulls them from the JSON and into a DataFrame
-def transform_weather(data):
+#takes the 7 fields we want from the weather API and the country/state deatils from the geo API and pulls them from the API JSONs and into a DataFrame
+def transform_weather(data_A, data_B):
     weather_events = {
-        "city": data["name"],
-        "temperature": data["main"]["temp"],
-        "pressure": data["main"]["pressure"],
-        "humidity": data["main"]["humidity"],
-        "weather_description": data["weather"][0]["description"],
-        "wind_speed": data["wind"]["speed"],
-        "timestamp": datetime.fromtimestamp(data["dt"])
+        "city_name": data_A["name"],
+        "temperature": data_A["main"]["temp"],
+        "pressure": data_A["main"]["pressure"],
+        "humidity": data_A["main"]["humidity"],
+        "weather_description": data_A["weather"][0]["description"],
+        "wind_speed": data_A["wind"]["speed"],
+        "timestamp": datetime.fromtimestamp(data_A["dt"]),
+        "country": data_B[0]["country"],
+        "state": data_B[0]["state"]
     }
     weather_table = pd.DataFrame([weather_events])
     return weather_table
 
-#calls the function with the variable below only when the file itself is ran. As opposed to being imported as intended
+#calls the function with the variables below only when the file itself is ran. As opposed to being imported as intended
 if __name__ == "__main__":
-    sample = {
+    sample_A = {
         "name": "Dallas",
         "main": {"temp": 76, "pressure": 1012, "humidity": 42},
         "weather": [{"description": "mostly cloudy"}],
         "wind": {"speed": 2.99},
         "dt": 1777578140
         }
-    
-    print(transform_weather(sample))
+    sample_B = [{
+        "country": "US",
+        "state": "Missouri"
+    }]
+    print(transform_weather(sample_A, sample_B))
