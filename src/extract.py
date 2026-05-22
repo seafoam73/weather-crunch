f376bcab3681b24008f92649738d07cb61b19047
seafoam73 <jgilmore2012@gmail.com>
2026-05-21 22:08:16 -0500

    fixed reversal of geo JSON and weather JSON, revised comments

--- src/extract.py
+++ src/extract.py
@@ -1,4 +1,4 @@
-#pulls the current weather for 4 of the most populated cities in MO & its state capital, and pulls weather information about those cities at that moment
+#pulls 5 large Missouri cities including the state capital, and pulls the current weather information from those cities
 import requests
 import json
 import os
@@ -24,7 +24,7 @@ def url_feed(x):
         city_url.append(f"http://api.openweathermap.org/geo/1.0/direct?q={i['City']},{i['State']},{i['Country']}&limit=1&appid={API_KEY}")
     return city_url
 
-#uses the lat/lon from url_feed to create the weather api
+#each city's lat/lon to build the weather URLs.
 def lat_lon(data):
     url_plug = []
     for i in data:
@@ -35,19 +35,21 @@ def lat_lon(data):
         url_plug.append(f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units=imperial")
     return url_plug
 
-#takes the geo URLs and the city weather URls, combines them and puts all the data into a list
+#takes the geo URLs and the city weather URls, returns both, geo as a list of list and weather as a list of dictionaries
 def extract_weather(x, y):
-    data_a =[]
-    data_b =[]
+    data_geo =[]
+    data_weather =[]
     for a, b in zip(x, y):
         response_a = requests.get(a)
         response_b = requests.get(b)
         combined_data_a = response_a.json()
         combined_data_b = response_b.json()
-        data_a.append(combined_data_a)
-        data_b.append(combined_data_b)
-    return data_a, data_b
+        data_geo.append(combined_data_a)
+        data_weather.append(combined_data_b)
+    return data_geo, data_weather
+    
 
 #only run extract_weather() if this file is being run directly. The function only fires when you explicitly run python src/extract.py.
 if __name__ == "__main__":
     extract_weather(url_feed(missouri_prime), lat_lon(url_feed(missouri_prime)))
+    
