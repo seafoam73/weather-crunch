32a713d760e69c29415f38de1d290a35d9b46691
seafoam73 <jgilmore2012@gmail.com>
2026-05-21 22:13:51 -0500

    fixed reversal of geo JSON and weather JSON, revised comments

--- src/transform.py
+++ src/transform.py
@@ -1,18 +1,18 @@
 import pandas as pd
 from datetime import datetime
 
-#takes the 7 fields we want from the weather API and the country/state deatils from the geo API and pulls them from the API JSONs and into a DataFrame
-def transform_weather(data_A, data_B):
+#takes the 7 fields we want from the weather API and the country/state details from the geo API, and pulls them from the API JSONs and into a DataFrame
+def transform_weather(data_geo, data_weather):
     weather_events = {
-        "city_name": data_A["name"],
-        "temperature": data_A["main"]["temp"],
-        "pressure": data_A["main"]["pressure"],
-        "humidity": data_A["main"]["humidity"],
-        "weather_name": data_A["weather"][0]["description"],
-        "wind_speed": data_A["wind"]["speed"],
-        "full_datetime": datetime.fromtimestamp(data_A["dt"]),
-        "country": data_B[0]["country"],
-        "state": data_B[0]["state"]
+        "city_name": data_weather["name"],
+        "temperature": data_weather["main"]["temp"],
+        "pressure": data_weather["main"]["pressure"],
+        "humidity": data_weather["main"]["humidity"],
+        "weather_name": data_weather["weather"][0]["description"],
+        "wind_speed": data_weather["wind"]["speed"],
+        "full_datetime": datetime.fromtimestamp(data_weather["dt"]),
+        "country": data_geo[0]["country"],
+        "state": data_geo[0]["state"]
     }
     weather_table = pd.DataFrame([weather_events])
     weather_table['year'] = weather_table['full_datetime'].dt.year
@@ -21,7 +21,7 @@ def transform_weather(data_A, data_B):
     weather_table['hour'] = weather_table['full_datetime'].dt.hour
     return weather_table
 
-#iterates through each city weather list, and city's state/country list and appends them to a master DataFrame
+#iterates through each city weather list, and city's state/country list and appends the called items to a master DataFrame
 def iterate_weather(x, y):
     frames = []
     for a, b in zip(x, y):
@@ -32,7 +32,7 @@ def iterate_weather(x, y):
 
 #calls the function with the variables below only when the file itself is ran. As opposed to being imported as intended
 if __name__ == "__main__":
-    sample_A = [{
+    sample_weather = [{
         "name": "Dallas",
         "main": {"temp": 76, "pressure": 1012, "humidity": 42},
         "weather": [{"description": "mostly cloudy"}],
@@ -47,7 +47,7 @@ if __name__ == "__main__":
         "dt": 1887578140
         }
         ]
-    sample_B = [[{
+    sample_geo = [[{
         "country": "US",
         "state": "Texas"
     }],
@@ -55,4 +55,4 @@ if __name__ == "__main__":
        "country": "US",
         "state": "Kentucky" 
     }]]
-    print(iterate_weather(sample_A, sample_B))
+    print(iterate_weather(sample_geo, sample_weather))
